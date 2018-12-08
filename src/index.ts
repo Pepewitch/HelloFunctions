08e04b22ca3f6e3b1551096bcb33ba94dc9ddad9
pepe_witch <skpepe134@gmail.com>
2018-12-08 11:21:06 +0700

    initialize firestore

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import express from "express";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import express from 'express';
+import { Firestore } from '@google-cloud/firestore';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+const firestore = new Firestore();
+firestore.settings({ timestampsInSnapshots: true });
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).send({ message: "Hello World" });
+app.get('/', (req, res) => {
+  return res.status(200).send({ message: 'Hello World' });
 });
 
-app.get("/ping", (req, res) => {
-  return res.status(200).send({ message: "Pong" });
-});
-
-app.post("/ping2", (req, res) => {
-  return res.status(200).send({ message: "POST Pong" });
+app.get('/db', async (req, res) => {
+  const test = firestore.collection('test');
+  (await test.where('a', '==', 'eiei').get()).forEach(e => {
+    console.log(e.data());
+  });
+  return res.status(200).send({ message: 'GET snapshot' });
 });
 
 export const api = functions.https.onRequest(app);
